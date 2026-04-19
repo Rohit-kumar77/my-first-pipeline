f78fc863d9e8567f4d723e25ef809aaa6b121163
Rohit Kumar <Rohitops007@gmail.com>
2026-04-19 19:26:28 +0530

    first terraform pipeline commit 7

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_server" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = "ami-07062e2a343acc423"
   instance_type = "t2.micro"
 
   tags = {
