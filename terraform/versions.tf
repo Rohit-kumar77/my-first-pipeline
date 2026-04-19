959ad804c0dae1783d52d4aafb52352defc92f0b
Rohit Kumar <Rohitops007@gmail.com>
2026-04-19 19:09:16 +0530

    first terraform pipeline commit 2

---
+++ terraform/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket         = "rohit-kumarops"   # ← your bucket name
+    key            = "my-first-pipeline/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
